c9e259f6a892f5054de73f96474ebe634bcc0bcd
MDNET-Developer <qarabagli.murad@mail.ru>
2022-10-14 15:00:59 +0400

    some busine_dataaccsess_update

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,45 @@
+﻿using Business.Abstract;
+using DataAccsess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    internal class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+        public void Add(Product product)
+        {
+            _productDal.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _productDal.Delete(product);
+        }
+
+        public Product GetById(int id)
+        {
+          return  _productDal.Get(x=>x.ProductId==id);
+        }
+
+        public List<Product> GetList()
+        {
+            return _productDal.Getlist().ToList();
+        }
+
+        public List<Product> GetListbyCategoryId(int categoryId)
+        {
+            return _productDal.Getlist(x=>x.CategoryId==categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            _productDal.Update(product);
+        }
+    }
+}
--- Core/EfEntityGenericBase/EfRepositoryBaseGeneric.cs
+++ Core/EfEntityGenericBase/EfRepositoryBaseGeneric.cs
@@ -19,28 +19,46 @@ namespace Core.EfEntityGenericBase
         {
             using (var context = new TContext())
             {
-
+                context.Entry(entity).State = EntityState.Added;
+                context.SaveChanges();
             };
         }
 
         public void Delete(TEntity entity)
         {
-            throw new NotImplementedException();
+            using (var context = new TContext())
+            {
+                context.Entry(entity).State = EntityState.Deleted;
+                context.SaveChanges();
+            };
         }
 
         public TEntity Get(Expression<Func<TEntity, bool>> filter)
         {
-            throw new NotImplementedException();
+            using (var context = new TContext())
+            {
+                return context.Set<TEntity>().FirstOrDefault(filter);
+
+            };
         }
 
         public IList<TEntity> Getlist(Expression<Func<TEntity, bool>> filter = null)
         {
-            throw new NotImplementedException();
+            using (var context = new TContext())
+            {
+                return filter==null? 
+                context.Set<TEntity>().ToList()
+                :context.Set<TEntity>().Where(filter).ToList();
+            };
         }
 
         public void Update(TEntity entity)
         {
-            throw new NotImplementedException();
+            using (var context = new TContext())
+            {
+                context.Entry(entity).State = EntityState.Modified;
+                context.SaveChanges();
+            };
         }
     }
 }
